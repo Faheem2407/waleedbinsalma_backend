97f98396abb9eb9d48edf64fbed96ed27e3f2b35
Rhishi-kesh <reshikash300@gmail.com>
2025-06-25 12:47:23 +0600

    Connect Account API Check

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
         $user = User::where('id', auth()->user()->id)->first();
 
         if ($user->role == "business") {
-            $user = User::where('id', auth()->user()->id)->with('businessProfile.onlineStore', 'businessProfile.businessDocument', 'businessProfile.businessServices')->first();
+            $user = User::where('id', auth()->user()->id)->with('businessProfile.onlineStore', 'businessProfile.businessDocument', 'businessProfile.businessServices', 'businessProfile.bankDetail')->first();
         }
         if (!$user) {
             return $this->error([], 'User Not Found', 404);
