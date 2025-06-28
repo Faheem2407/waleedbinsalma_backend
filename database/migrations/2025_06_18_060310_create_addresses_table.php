a5ed5a30c1aacfc61b9cea46305917a98ea5cfcc
Faheem2407 <faheem24072000@gmail.com>
2025-06-28 17:03:40 +0600

    api update

--- database/migrations/2025_06_18_060310_create_addresses_table.php
+++ database/migrations/2025_06_18_060310_create_addresses_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('address');
+            $table->string('country')->nullable();
+            $table->string('street')->nullable();
+            $table->string('city')->nullable();
             $table->timestamps();
         });
     }
