52a38d14f99772284902a7508da2869806f8911b
Faheem2407 <faheem24072000@gmail.com>
2025-07-02 16:37:39 +0600

    update

--- app/Http/Controllers/Api/Catalog/ProductController.php
+++ app/Http/Controllers/Api/Catalog/ProductController.php
@@ -116,7 +116,7 @@ class ProductController extends Controller
             'name' => 'sometimes|required|string',
             'barcode' => 'nullable|string',
             'measure' => 'sometimes|required|string',
-            'amount' => 'sometimes|required|numeric|min:0',
+            'amount' => 'sometimes|required|integer|min:0',
             'short_description' => 'nullable|string',
             'description' => 'nullable|string',
             'supply_price' => 'sometimes|required|numeric|min:0',
